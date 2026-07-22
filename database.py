246f2bff6e005e65e02628c8b49717cde557a4ea
Akshay Chandra N <161806342+nAkshay11@users.noreply.github.com>
2026-07-22 17:00:51 +0530

    database.py
    Fix database connection to use secrets

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import streamlit as st
 
-engine = create_engine(
-    "postgresql://postgres:@localhost:5432/InsightFlow"
-)
+@st.cache_resource
+def get_engine():
+    db_url = st.secrets["DATABASE_URL"]
+    return create_engine(db_url)
 
 def run_query(query):
+    engine = get_engine()
     return pd.read_sql(query, engine)
